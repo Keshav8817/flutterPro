8eb5ad12e508f0655713d6b31516a1a44c8bacf0
Keshav8817 <keshav.sharma@gigatorb.com>
2023-06-05 14:20:49 +0530

    Day 4 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,7 +6,28 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(child: Text("Login Page"),),
+      color: Colors.white,
+      child: Column(children: [
+        Image.asset("assets/images/login.png",fit: BoxFit.cover,),
+      Padding(padding: const EdgeInsets.symmetric(vertical: 16 , horizontal: 32),
+      child: Column(
+        children: [
+            TextFormField(
+          decoration: InputDecoration(hintText: "Enter User Name",labelText: "User Name",),
+        ),
+        TextFormField(
+          obscureText: true,
+          decoration: InputDecoration(hintText: "Enter Password",labelText: "Password",),
+        ) ,
+        ElevatedButton(
+          child: Text("Login"),
+          onPressed: () {
+            print("WelCome to port");
+          },
+          )
+        ],
+      ),),
+      ],)
     );
   }
 }
